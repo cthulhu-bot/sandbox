952b9ae60f716f2090244090940a4d0c8f136d4f
Josh Stupplebeen <jpstup@gmail.com>
2015-11-17 00:58:17 -0700

    Attempting to work

--- bowling-game/src/BowlingGame.js
+++ bowling-game/src/BowlingGame.js
@@ -5,11 +5,22 @@ var Game = function() {
 };
 
 Game.prototype.roll = function(pins) {
-  this.score += pins;
   this.rolls[this.currentRoll++] = pins;
 };
 
 Game.prototype.score = function() {
+  this.score = 0;
+  i = 0;
+  for (frame=0; frame<10; frame++) {
+    console.log("Hi There");
+    if (this.rolls[i] + this.rolls[i+1] === 10) {
+      this.score += this.rolls[i+2];
+      i += 2;
+    } else {
+      this.score += this.rolls[i] + this.rolls[i+1];
+      i += 2;
+    }
+  }
   return this.score;
 };
 
