b981412282bce026201152efe8d29605c3ba4e07
Josh Stupplebeen <jpstup@gmail.com>
2015-11-22 11:37:19 -0700

    Authors page navigation complete

---
+++ react/react-and-flux/src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+  authors:
+  [
+    {
+      id: 'tony-stark',
+      firstName: 'Tony',
+      lastName: 'Stark'
+    },
+    {
+      id: 'bruce-banner',
+      firstName: 'Bruce',
+      lastName: 'Banner'
+    },
+    {
+      id: 'steve-rogers',
+      firstName: 'Steve',
+      lastName: 'Rogers'
+    }
+  ]
+};
